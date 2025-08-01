bdab36eb239ae458c95427fae30143c8ef11e19e
aleksandr <azhdankov@alfabank.ru>
2025-08-01 11:30:06 +0300

    ini

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,13 +11,15 @@ const keycloakConfig: KeycloakConfig = {
 
 const keycloak = new Keycloak(keycloakConfig);
 
-keycloak.init({
-  pkceMethod: 'S256'
-});
-
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider 
+      authClient={keycloak}
+      initOptions={{
+        pkceMethod: 'S256',
+        checkLoginIframe: false
+      }}
+    >
       <div className="App">
         <ReportPage />
       </div>
